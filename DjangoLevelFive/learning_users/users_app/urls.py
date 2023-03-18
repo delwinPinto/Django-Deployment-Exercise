21b6335274bd6d75c412ff4a6c2847246a9f6e29
Delwin <pintodelwin22@gmail.com>
2023-03-18 17:54:29 +0530

    first

---
+++ DjangoLevelFive/learning_users/users_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from users_app import views
+
+app_name='users_app'
+urlpatterns=[
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+]
